1d0655c09919eda05a65520b9d871235f1033773
yogesh Kumar <iamyogeshkumar21@gmail.com>
2022-10-25 20:23:47 +0530

    dec

--- React/react/src/Components/Counteruptoten/Counteruptoten.jsx
+++ React/react/src/Components/Counteruptoten/Counteruptoten.jsx
@@ -6,13 +6,15 @@ const Counteruptoten = () => {
   const inc =()=>{
     setCount(count+1)
   }
-
+  const dec =()=>{
+    setCount(count-1)
+  }
   
   return (
     <div>
       <h1>{count}</h1>
       <button onClick={inc}>increment</button>
-      <button>Decrement</button>
+      <button onClick={dec}>Decrement</button>
     </div>
 
   )
