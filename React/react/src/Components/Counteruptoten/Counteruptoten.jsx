88dc369673a1dd36f2f8b043c15a0e9fd04a9fc6
yogesh Kumar <iamyogeshkumar21@gmail.com>
2022-10-25 20:22:08 +0530

    inc

--- React/react/src/Components/Counteruptoten/Counteruptoten.jsx
+++ React/react/src/Components/Counteruptoten/Counteruptoten.jsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react'
 
 const Counteruptoten = () => {
-  const [count,setCount]=useState(0)
+  const [count,setCount]=useState(0);
+
+  const inc =()=>{
+    setCount(count+1)
+  }
+
+  
   return (
     <div>
       <h1>{count}</h1>
-      <button>increment</button>
+      <button onClick={inc}>increment</button>
       <button>Decrement</button>
     </div>
 
