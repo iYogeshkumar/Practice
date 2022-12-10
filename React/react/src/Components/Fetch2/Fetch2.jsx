507f4ac5aa7b71161eb0ccf5c049763bca0a443c
yogesh Kumar <iamyogeshkumar21@gmail.com>
2022-12-11 01:37:34 +0530

    fetch using async await

--- React/react/src/Components/Fetch2/Fetch2.jsx
+++ React/react/src/Components/Fetch2/Fetch2.jsx
@@ -31,9 +31,12 @@
 // }
 
 // export default Fetch2
-//////////////end Method 1- Fetch Api  /////////////////////////////////////
+//////////////   end Method 1- Fetch Api  /////////////////////////////////////
+
 
 // ---------------------------------------------------------------------------------------------------------//
+
+
 
 
 /////////////          Method 2 - Using Axios library        /////////////////////////////////
@@ -43,32 +46,110 @@
 // we have to convert the result to a JSON object but in Axios it already returns the result as a JSON object,
 //  so we don’t need to convert it.}
 
+// import axios from 'axios'
+// import React from 'react'
+// import { useState } from 'react'
+// import { useEffect } from 'react'
+
+// const Fetch2 = () => {
+//     const [user,setUser]= useState([])
+//     const getData= () => {
+//         return axios.get("https://api.github.com/users")
+//         .then(response => setUser(response.data))
+//     }
+//     useEffect(() => {
+//         getData()
+//     },[])
+//   return (
+    
+//     <div>
+//       {user.map((el)=>(
+//         <div key={el.id}>
+//             <h1>{el.login}</h1>
+//             <img src={el.avatar_url} width={"100px"} alt={el.image}/>
+//         </div>
+//       )
+//       )}
+//     </div>
+//   )
+// }
+
+// export default Fetch2
+
+/////////////    end      Method 2 - Using Axios library        /////////////////////////////////
+
+
+// ---------------------------------------------------------------------------------------------------------//
+
+
+
+
+
+///////////////////          Method 3 - Using Async Await         /////////////////////////////////
+
 import axios from 'axios'
-import React from 'react'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import { useEffect } from 'react'
 
 const Fetch2 = () => {
     const [user,setUser]= useState([])
-    const getData= () => {
-        return axios.get("https://api.github.com/users")
-        .then(response => setUser(response.data))
-    }
-    useEffect(() => {
-        getData()
-    },[])
-  return (
+
+    // async function fetchData() {
+    //     try {
+    //     const response = await fetch("https://api.github.com/users") 
+    //     const userdata = response.json()
+    //     return userdata
+        
+    //     } catch (error) {
+    //       console.error(error);
+    //     }
+    //   }
+
+
+    // //   fetchData().then((res) => {
+    // //     setUser(res)
+    // //     // console.log(res)
+    // //   })
+      
+    // ///////////OR/////////
+
+    //   useEffect(() => {
+    //     fetchData().then((res) => {
+    //         setUser(res)
+    //         // console.log(res)
+    //       })
+    //   },[])
+
+
+
+    //////////////USING AXIOS IN Async Await /////////////////////////////
+    async function fetchData() {
+        try {
+          const response = await axios.get("https://api.github.com/users")
+          setUser(response.data)
+        } catch (error) {
+          console.error(error);
+        }
+      }
     
+      useEffect(() => {
+        fetchData();
+      },[])
+  return (
     <div>
-      {user.map((el)=>(
-        <div key={el.id}>
-            <h1>{el.login}</h1>
-            <img src={el.avatar_url} width={"100px"} alt={el.image}/>
-        </div>
-      )
-      )}
-    </div>
+    {user.map((el)=>(
+      <div key={el.id}>
+          <h1>{el.login}</h1>
+          <img src={el.avatar_url} width={"100px"} alt={el.image}/>
+      </div>
+   )
+  )}
+</div>
   )
 }
 
 export default Fetch2
+
+
+
+/////////////    END of      Method 3 - Using Async Await         /////////////////////////////////
