b50dae340cb86caffa222117c183e8f4f9d6968b
yogesh Kumar <iamyogeshkumar21@gmail.com>
2022-12-20 16:50:39 +0530

    Update Display.jsx

--- React/react/src/Components/Fetchanddisplayinnextpage/Display.jsx
+++ React/react/src/Components/Fetchanddisplayinnextpage/Display.jsx
@@ -1,13 +1,13 @@
-// import React from 'react'
-// import './Display.css'
-// const Display = ({name,email,username}) => {
-//   return (
-//     <div className='box'>
-//       <p>{name}</p>
-//       <p>{username}</p>
-//       <p>{email}</p>
-//     </div>
-//   )
-// }
+import React from 'react'
+import './Display.css'
+const Display = ({name,email,username}) => {
+  return (
+    <div className='box'>
+      <p>{name}</p>
+      <p>{username}</p>
+      <p>{email}</p>
+    </div>
+  )
+}
 
-// export default Display
+export default Display
