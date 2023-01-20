620cd67f3663304c8da7f189cf44e176910d5517
yogesh Kumar <iamyogeshkumar21@gmail.com>
2023-01-20 23:43:06 +0530

    Update index.js

--- JavaScript/14.spread and rest operator/index.js
+++ JavaScript/14.spread and rest operator/index.js
@@ -5,4 +5,9 @@ var arr2= [...arr1,4,5]
 
 console.log(arr2) //[1,2,3,4,5]
 
-//rest 
+//rest operator - when the spread operator is used as a parameter, it is known as rest operator
+
+function x(b,...a){
+    console.log(b,a)
+}
+x(5,6,7)
