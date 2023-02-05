c1798423e3b8698134cbfdef2354cc16c93a7ced
yogesh Kumar <iamyogeshkumar21@gmail.com>
2023-02-05 17:32:11 +0530

    practice

--- DSA/removeDuplicate.js
+++ DSA/removeDuplicate.js
@@ -6,7 +6,7 @@
 //     arr.sort()
 //     var bag=[]
 //     for(var i=0; i<arr.length; i++){
-//         if(arr[i]=arr[i+1]){
+//         if(arr[i]!=arr[i+1]){
 //             bag.push(arr[i])
 //         }
 //     }
@@ -22,8 +22,27 @@
 
 ///////print dublicate element from array
 
+// var arr=[1,2,5,6,2,3,4,5,]
+
+// var duplicate=arr.filter((item,index) =>index!==arr.indexOf(item))
+// console.log(duplicate) //[2,5]
+
+
+
+// 2nd method
 var arr=[1,2,5,6,2,3,4,5,]
 
-var duplicate=arr.filter((item,index) =>index!==arr.indexOf(item))
-console.log(duplicate) //[2,5]
-
+function removeDuplicate(arr) {
+   
+    var bag=[]
+    for(var i=0; i<arr.length; i++){
+        for(var j=i+1; j<arr.length; j++){
+            if(arr[i]==arr[j]){
+            bag.push(arr[i])
+        }
+        }
+        
+    }
+    console.log(bag) //[1,2,3,4,5,6]
+}
+removeDuplicate(arr)
